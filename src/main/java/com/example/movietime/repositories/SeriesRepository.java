e22ab1bc3035002a7d1832125639827ab11403f8
Teodora Yuliyanova <teodorayuliyanova@gmail.com>
2023-04-08 11:02:47 +0100

    Delete src directory

--- src/main/java/com/example/movietime/repositories/SeriesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.movietime.repositories;
-
-import com.example.movietime.domain.entities.SeriesEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeriesRepository extends JpaRepository<SeriesEntity,Long> {
-}
