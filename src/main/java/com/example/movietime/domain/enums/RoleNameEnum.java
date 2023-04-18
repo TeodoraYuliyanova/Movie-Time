dac737c3a8dcdaa76acf35f0e547d42852968741
Teodora Yuliyanova <teodorayuliyanova@gmail.com>
2023-04-18 23:41:37 +0100

    Delete src directory

--- src/main/java/com/example/movietime/domain/enums/RoleNameEnum.java
+++
@@ -1,6 +0,0 @@
-package com.example.movietime.domain.enums;
-
-public enum RoleNameEnum {
-
-    USER,ADMIN
-}
--- src/main/java/com/example/movietime/repositories/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.movietime.repositories;
-
-import com.example.movietime.domain.entities.MovieEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovieRepository extends JpaRepository<MovieEntity,Long> {
-}
